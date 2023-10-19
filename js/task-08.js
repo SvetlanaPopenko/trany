d9a4b253476627101d3252e8cef24ef2baea9805
SvetlanaPopenko <spopenko02@gmail.com>
2023-10-20 00:04:50 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm(evt) {
+  evt.preventDefault();
+  const form = {};
+  const { email, password } = evt.currentTarget.elements;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля мають бути заповнені");
+  }
+
+  form[email.name] = email.value;
+  form[password.name] = password.value;
+  console.log(form);
+
+  registerForm.reset();
+}
