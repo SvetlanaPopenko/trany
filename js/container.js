fe266119a38886ea276ee6d2200fdf66f930aa99
SvetlanaPopenko <spopenko02@gmail.com>
2023-10-29 00:50:43 +0300

    Update container.js

--- js/container.js
+++ js/container.js
@@ -1,15 +1,33 @@
-const container = document.querySelector('.js-content');
+const container = document.querySelector(".js-content");
 
-let markup = '';
+const wins = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+let player = "x";
+let markup = "";
+const historyx = [];
+const history0 = [];
 
-for (let i = 0; i < 9; i += 1){
-    markup+=`<div class="item"></div>`
+for (let i = 1; i < 10; i += 1) {
+  markup += `<div class="item js-item" data-id=${i}></div>`;
 }
 
 container.innerHTML = markup;
 
-container.addEventListener('click', onclick);
+container.addEventListener("click", onClick);
 
 function onClick(evt) {
-    
+  const { target } = evt;
+  if (!target.classList.contains("js-item") || target.textContent) {
+    return;
+  }
+  target.textContent = player;
+  player = player === "x" ? "0" : "x";
 }
