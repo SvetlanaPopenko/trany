7079527c05a09a8e195befc47eac3e349a10829f
SvetlanaPopenko <spopenko02@gmail.com>
2023-11-09 00:01:30 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const markup=galleryItems.map(({preview, original, description}) =>`<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('');
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+const instance = basicLightbox.create(`
+    <h1>Dynamic Content</h1>
+    <p>You can set the content of the lightbox with JS.</p>
+`)
+
+console.log(instance);
+instance.show(() => console.log('lightbox now visible'));
