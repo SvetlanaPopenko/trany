4f43b2711f1262bf0afe17f86e024a48007a2cc6
SvetlanaPopenko <spopenko02@gmail.com>
2023-11-15 00:55:53 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,11 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
 
-const galleryRef = document.querySelector('.gallery');
-
-const markup=galleryItems.map(({preview, original, description}) =>`<li class="gallery__item">
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -14,14 +14,38 @@ const markup=galleryItems.map(({preview, original, description}) =>`<li class="g
       alt="${description}"
     />
   </a>
-</li>`).join('');
+</li>`
+  )
+  .join("");
 
-galleryRef.insertAdjacentHTML('beforeend', markup);
+galleryRef.insertAdjacentHTML("beforeend", markup);
 
-const instance = basicLightbox.create(`
-    <h1>Dynamic Content</h1>
-    <p>You can set the content of the lightbox with JS.</p>
-`)
+galleryRef.addEventListener("click", onClick);
 
-console.log(instance);
-instance.show(() => console.log('lightbox now visible'));
+function onClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `
+    <div><img src="${evt.target.dataset.source}" width="1200"></div>
+    `,
+    {
+      onShow: (instance) => {
+        galleryRef.addEventListener("keydown", onEscape);
+      },
+      onClose: (instance) => {
+        galleryRef.removeEventListener("keydown", onEscape);
+      },
+    }
+  );
+  instance.show();
+
+  function onEscape(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+}
